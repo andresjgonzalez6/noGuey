391ea985b73ac71ac2f2cbac0c7d39af7afb4cb4
Rex <roydavis99.rd@gmail.com>
2018-10-25 20:17:15 -0700

    src first attempt

--- public/pages/scr/scr.js
+++ public/pages/scr/scr.js
@@ -1,4 +1,4 @@
-$("#tables-btn").on("click", function() {
+$("#tables-btn").on("click", function () {
     //var searchedCharacter = $("#character-search").val().trim();
 
     // Using a RegEx Pattern to remove spaces from searchedCharacter
@@ -6,18 +6,19 @@ $("#tables-btn").on("click", function() {
     //searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
 
     // QUESTION: What does $.get do? What are the parameters it is expecting?
-    $.get("/tables/" + searchedCharacter, function(data) {
-      console.log(data);
-      if (data) {
-        $("#stats").show();
-        $("#name").text(data.name);
-        $("#role").text(data.role);
-        $("#age").text(data.age);
-        $("#force-points").text(data.strengthPoints);
-      }
-      else {
-        $("#name").text("They are not an avenger, Check the Revengers. Your character was not found.");
-        $("#stats").hide();
-      }
+    $.get("/tables/", function (data) {
+        console.log(data);
+        console.log(data);
+        /* if (data) {
+          $("#stats").show();
+          $("#name").text(data.name);
+          $("#role").text(data.role);
+          $("#age").text(data.age);
+          $("#force-points").text(data.strengthPoints);
+        }
+        else {
+          $("#name").text("They are not an avenger, Check the Revengers. Your character was not found.");
+          $("#stats").hide();
+        } */
     });
-  });
+});
